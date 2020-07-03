a7aaeeff5a3f88ff41502b7d5536f7c58dbbf5ce
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-03 13:12:03 -0500

    published v1

--- src/griddy-api.ts
+++ src/griddy-api.ts
@@ -3,35 +3,35 @@ import request from 'request-promise-native';
 const apiEndpoint = 'https://app.gogriddy.com/api/v1/insights/getnow';
 
 const getData = async (settlement_point: string) => {
-    const body = {
-        settlement_point
-    };
-    return (await request({
-        uri: apiEndpoint,
-        method: 'POST',
-        body,
-        json: true
-    })) as GriddyResponse;
+  const body = {
+    settlement_point
+  };
+  return (await request({
+    uri: apiEndpoint,
+    method: 'POST',
+    body,
+    json: true
+  })) as GriddyResponse;
 };
 
 interface GriddyItem {
-    date: Date;
-    hour_num: number;
-    price_type: String;
-    price_ckwh: number;
-    value_score: number;
-    mean_price_ckwh: number;
-    diff_mean_ckwh: number;
-    high_ckwh: number;
-    low_ckwh: number;
-    std_dev_ckwh: number;
-    price_display: number;
-    date_local_tz: Date;
+  date: Date;
+  hour_num: number;
+  price_type: string;
+  price_ckwh: number;
+  value_score: number;
+  mean_price_ckwh: number;
+  diff_mean_ckwh: number;
+  high_ckwh: number;
+  low_ckwh: number;
+  std_dev_ckwh: number;
+  price_display: number;
+  date_local_tz: Date;
 }
 interface GriddyResponse {
-    now: GriddyItem;
-    forecast: GriddyItem[];
-    seconds_until_refresh: number;
+  now: GriddyItem;
+  forecast: GriddyItem[];
+  seconds_until_refresh: number;
 }
 
 export { getData, GriddyResponse };
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,9 +1,4 @@
-import {
-    Service,
-    PlatformAccessory,
-    CharacteristicSetCallback,
-    CharacteristicGetCallback
-} from 'homebridge';
+import { Service, PlatformAccessory, CharacteristicSetCallback, CharacteristicGetCallback } from 'homebridge';
 import { GriddyResponse, getData } from './griddy-api';
 import { ExampleHomebridgePlatform } from './platform';
 
@@ -13,156 +8,117 @@ import { ExampleHomebridgePlatform } from './platform';
  * Each accessory may expose multiple services of different service types.
  */
 export class ExamplePlatformAccessory {
-    private priceService: Service;
-    private intensityService: Service;
-    private lowPriceService: Service;
-    private latestGriddyData: GriddyResponse | undefined = undefined;
-    /**
-     * These are just used to create a working example
-     * You should implement your own code to track the state of your accessory
-     */
+  private priceService: Service;
+  private intensityService: Service;
+  private lowPriceService: Service;
+  private latestGriddyData: GriddyResponse | undefined = undefined;
+  /**
+   * These are just used to create a working example
+   * You should implement your own code to track the state of your accessory
+   */
 
-    constructor(
-        private readonly platform: ExampleHomebridgePlatform,
-        private readonly accessory: PlatformAccessory
-    ) {
-        // set accessory information
-        console.log(this.platform.config);
-        this.accessory
-            .getService(this.platform.Service.AccessoryInformation)!
-            .setCharacteristic(
-                this.platform.Characteristic.Manufacturer,
-                'Default-Manufacturer'
-            )
-            .setCharacteristic(
-                this.platform.Characteristic.Model,
-                'Default-Model'
-            )
-            .setCharacteristic(
-                this.platform.Characteristic.SerialNumber,
-                'Default-Serial'
-            );
+  constructor(private readonly platform: ExampleHomebridgePlatform, private readonly accessory: PlatformAccessory) {
+    // set accessory information
+    this.platform.log.debug('config', this.platform.config);
 
-        // we use a fake light sensor to represent the real-time price in cents
-        this.priceService =
-            this.accessory.getService(this.platform.Service.LightSensor) ||
-            this.accessory.addService(this.platform.Service.LightSensor);
-        // intensity is the price as a percentage of the day's range.  Low means it's cheap compared to the day's prices.  This device turns "on" when the price is high.
-        this.intensityService =
-            this.accessory.getService(this.platform.Service.Lightbulb) ||
-            this.accessory.addService(this.platform.Service.Lightbulb);
-        // also uses the intensity, but turns on when the price is "low"
-        this.lowPriceService =
-            this.accessory.getService(this.platform.Service.Switch) ||
-            this.accessory.addService(this.platform.Service.Switch);
+    this.accessory
+      .getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
+      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
 
-        // set the service name, this is what is displayed as the default name on the Home app
-        // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-        this.priceService.setCharacteristic(
-            this.platform.Characteristic.Name,
-            // accessory.context.device.priceServiceName
-            'Griddy Price'
-        );
+    // we use a fake light sensor to represent the real-time price in cents
+    this.priceService =
+      this.accessory.getService(this.platform.Service.LightSensor) ||
+      this.accessory.addService(this.platform.Service.LightSensor);
+    // intensity is the price as a percentage of the day's range.
+    // Low means it's cheap compared to the day's prices.This device turns "on" when the price is high.
+    this.intensityService =
+      this.accessory.getService(this.platform.Service.Lightbulb) ||
+      this.accessory.addService(this.platform.Service.Lightbulb);
+    // also uses the intensity, but turns on when the price is "low"
+    this.lowPriceService =
+      this.accessory.getService(this.platform.Service.Switch) ||
+      this.accessory.addService(this.platform.Service.Switch);
 
-        this.intensityService.setCharacteristic(
-            this.platform.Characteristic.Name,
-            // accessory.context.device.intensityServiceName
-            'Griddy High Price'
-        );
-        this.lowPriceService.setCharacteristic(
-            this.platform.Characteristic.Name,
-            // accessory.context.device.intensityServiceName
-            'Griddy Low Price'
-        );
+    // set the service name, this is what is displayed as the default name on the Home app
+    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+    this.priceService.setCharacteristic(
+      this.platform.Characteristic.Name,
+      // accessory.context.device.priceServiceName
+      'Griddy Price'
+    );
 
-        // each service must implement at-minimum the "required characteristics" for the given service type
-        // see https://developers.homebridge.io/#/service/Lightbulb
+    this.intensityService.setCharacteristic(
+      this.platform.Characteristic.Name,
+      // accessory.context.device.intensityServiceName
+      'Griddy High Price'
+    );
+    this.lowPriceService.setCharacteristic(
+      this.platform.Characteristic.Name,
+      // accessory.context.device.intensityServiceName
+      'Griddy Low Price'
+    );
 
-        // register handlers for the On/Off Characteristic
-        this.priceService
-            .getCharacteristic(
-                this.platform.Characteristic.CurrentAmbientLightLevel
-            )
-            .on('get', this.getLevel.bind(this));
+    // each service must implement at-minimum the "required characteristics" for the given service type
+    // see https://developers.homebridge.io/#/service/Lightbulb
 
-        this.priceService
-            .getCharacteristic(this.platform.Characteristic.StatusActive)
-            .on('get', this.getStatus.bind(this)); // SET - bind to the 'setBrightness` method below
+    // register handlers for the On/Off Characteristic
+    this.priceService
+      .getCharacteristic(this.platform.Characteristic.CurrentAmbientLightLevel)
+      .on('get', this.getLevel.bind(this));
 
-        this.intensityService
-            .getCharacteristic(this.platform.Characteristic.Brightness)
-            .on('get', this.getIntensity.bind(this));
+    // SET - bind to the 'setBrightness` method below
+    this.priceService.getCharacteristic(this.platform.Characteristic.StatusActive).on('get', this.getStatus.bind(this));
 
-        this.intensityService
-            .getCharacteristic(this.platform.Characteristic.On)
-            .on('get', this.getIsHigh.bind(this));
+    this.intensityService
+      .getCharacteristic(this.platform.Characteristic.Brightness)
+      .on('get', this.getIntensity.bind(this));
 
-        this.lowPriceService
-            .getCharacteristic(this.platform.Characteristic.On)
-            .on('get', this.getIsLow.bind(this));
+    this.intensityService.getCharacteristic(this.platform.Characteristic.On).on('get', this.getIsHigh.bind(this));
 
-        this.update();
+    this.lowPriceService.getCharacteristic(this.platform.Characteristic.On).on('get', this.getIsLow.bind(this));
+
+    this.update();
+  }
+
+  async update() {
+    this.platform.log.debug('running update');
+    this.latestGriddyData = await getData(this.platform.config.zone);
+    this.platform.log.debug(`update finished. Next in ${this.latestGriddyData.seconds_until_refresh} seconds`);
+    this.platform.log.debug('latest data', this.latestGriddyData.now);
+    this.platform.log.debug(`intensity is ${this.calculateIntensity(this.latestGriddyData)}`);
+    if (this.latestGriddyData) {
+      // make it clear that we do have price data
+      this.priceService.updateCharacteristic(this.platform.Characteristic.StatusActive, true);
+      // sets the price on the price service so you can see it in Home
+      this.priceService.updateCharacteristic(
+        this.platform.Characteristic.CurrentAmbientLightLevel,
+        Math.round(this.latestGriddyData.now.price_ckwh * 1000) / 1000
+      );
+      // sets the intensity
+      this.intensityService.updateCharacteristic(
+        this.platform.Characteristic.Brightness,
+        this.calculateIntensity(this.latestGriddyData)
+      );
+      // flips the high price service to on
+      this.intensityService.updateCharacteristic(
+        this.platform.Characteristic.On,
+        this.defineHigh(this.latestGriddyData)
+      );
+      // flips the low price service to in
+      this.lowPriceService.updateCharacteristic(this.platform.Characteristic.On, this.defineLow(this.latestGriddyData));
+      setTimeout(async () => this.update(), this.latestGriddyData.seconds_until_refresh * 1000);
+    } else {
+      // we don't know anything so set things to off
+      this.priceService.updateCharacteristic(this.platform.Characteristic.StatusActive, false);
+      this.intensityService.updateCharacteristic(this.platform.Characteristic.On, false);
+      this.lowPriceService.updateCharacteristic(this.platform.Characteristic.On, false);
+      setTimeout(async () => this.update(), 10000);
     }
+  }
 
-    async update() {
-        console.log('running update');
-        this.latestGriddyData = await getData(this.platform.config.zone);
-        console.log(
-            `update finished. Next in ${this.latestGriddyData.seconds_until_refresh} seconds`
-        );
-        console.log(this.latestGriddyData.now);
-        console.log(
-            `intensity is ${this.calculateIntensity(this.latestGriddyData)}`
-        );
-        if (this.latestGriddyData) {
-            // make it clear that we do have price data
-            this.priceService.updateCharacteristic(
-                this.platform.Characteristic.StatusActive,
-                true
-            );
-            // sets the price on the price service so you can see it in Home
-            this.priceService.updateCharacteristic(
-                this.platform.Characteristic.CurrentAmbientLightLevel,
-                Math.round(this.latestGriddyData.now.price_ckwh * 1000) / 1000
-            );
-            // sets the intensity
-            this.intensityService.updateCharacteristic(
-                this.platform.Characteristic.Brightness,
-                this.calculateIntensity(this.latestGriddyData)
-            );
-            // flips the high price service to on
-            this.intensityService.updateCharacteristic(
-                this.platform.Characteristic.On,
-                this.defineHigh(this.latestGriddyData)
-            );
-            // flips the low price service to in
-            this.lowPriceService.updateCharacteristic(
-                this.platform.Characteristic.On,
-                this.defineLow(this.latestGriddyData)
-            );
-            setTimeout(
-                async () => this.update(),
-                this.latestGriddyData.seconds_until_refresh * 1000
-            );
-        } else {
-            // we don't know anything so set things to off
-            this.priceService.updateCharacteristic(
-                this.platform.Characteristic.StatusActive,
-                false
-            );
-            this.intensityService.updateCharacteristic(
-                this.platform.Characteristic.On,
-                false
-            );
-            this.lowPriceService.updateCharacteristic(
-                this.platform.Characteristic.On,
-                false
-            );
-            setTimeout(async () => this.update(), 10000);
-        }
-    }
-
-    /**
+  /**
    * Handle the "GET" requests from HomeKit
    * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
    * 
@@ -175,77 +131,68 @@ export class ExamplePlatformAccessory {
    * @example
    * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
    */
-    getStatus(callback: CharacteristicGetCallback) {
-        // you must call the callback function
-        // the first argument should be null if there were no errors
-        // the second argument should be the value to return
-        callback(null, this.latestGriddyData ? true : false);
-    }
+  getStatus(callback: CharacteristicGetCallback) {
+    // you must call the callback function
+    // the first argument should be null if there were no errors
+    // the second argument should be the value to return
+    callback(null, this.latestGriddyData ? true : false);
+  }
 
-    /**
-     * Handle "SET" requests from HomeKit
-     * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-     */
-    getLevel(callback: CharacteristicSetCallback) {
-        // you must call the callback function
-        if (this.latestGriddyData) {
-            callback(
-                null,
-                Math.round(this.latestGriddyData.now.price_ckwh * 1000) / 1000
-            );
-        } else {
-            callback(Error('No information available'));
-        }
+  /**
+   * Handle "SET" requests from HomeKit
+   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
+   */
+  getLevel(callback: CharacteristicSetCallback) {
+    // you must call the callback function
+    if (this.latestGriddyData) {
+      callback(null, Math.round(this.latestGriddyData.now.price_ckwh * 1000) / 1000);
+    } else {
+      callback(Error('No information available'));
     }
+  }
 
-    getIntensity(callback: CharacteristicSetCallback) {
-        // you must call the callback function
-        if (this.latestGriddyData) {
-            callback(null, this.calculateIntensity(this.latestGriddyData));
-        } else {
-            callback(Error('No information available'));
-        }
+  getIntensity(callback: CharacteristicSetCallback) {
+    // you must call the callback function
+    if (this.latestGriddyData) {
+      callback(null, this.calculateIntensity(this.latestGriddyData));
+    } else {
+      callback(Error('No information available'));
     }
+  }
 
-    getIsHigh(callback: CharacteristicSetCallback) {
-        if (this.latestGriddyData) {
-            callback(null, this.defineHigh(this.latestGriddyData));
-        } else {
-            callback(Error('No information available'));
-        }
+  getIsHigh(callback: CharacteristicSetCallback) {
+    if (this.latestGriddyData) {
+      callback(null, this.defineHigh(this.latestGriddyData));
+    } else {
+      callback(Error('No information available'));
     }
+  }
 
-    getIsLow(callback: CharacteristicSetCallback) {
-        if (this.latestGriddyData) {
-            callback(null, this.defineLow(this.latestGriddyData));
-        } else {
-            callback(Error('No information available'));
-        }
+  getIsLow(callback: CharacteristicSetCallback) {
+    if (this.latestGriddyData) {
+      callback(null, this.defineLow(this.latestGriddyData));
+    } else {
+      callback(Error('No information available'));
     }
+  }
 
-    calculateIntensity(data: GriddyResponse) {
-        return (
-            ((data.now.price_ckwh - data.now.low_ckwh) /
-                (data.now.high_ckwh - data.now.low_ckwh)) *
-            100
-        );
-    }
+  calculateIntensity(data: GriddyResponse) {
+    return ((data.now.price_ckwh - data.now.low_ckwh) / (data.now.high_ckwh - data.now.low_ckwh)) * 100;
+  }
 
-    defineLow(data: GriddyResponse) {
-        // if the curve is pretty flat but the price is cheap, let's go!
-        return (
-            data.now.price_ckwh < this.platform.config.lowPriceCents ||
-            this.calculateIntensity(data) <
-                this.platform.config.lowPricePercentage
-        );
-    }
+  defineLow(data: GriddyResponse) {
+    // if the curve is pretty flat but the price is cheap, let's go!
+    return (
+      data.now.price_ckwh < this.platform.config.lowPriceCents ||
+      this.calculateIntensity(data) < this.platform.config.lowPricePercentage
+    );
+  }
 
-    defineHigh(data: GriddyResponse) {
-        // if the curve is pretty flat, we don't want to call a high price event
-        return (
-            data.now.price_ckwh > this.platform.config.highPriceCents &&
-            this.calculateIntensity(data) >
-                this.platform.config.highPricePercentage
-        );
-    }
+  defineHigh(data: GriddyResponse) {
+    // if the curve is pretty flat, we don't want to call a high price event
+    return (
+      data.now.price_ckwh > this.platform.config.highPriceCents &&
+      this.calculateIntensity(data) > this.platform.config.highPricePercentage
+    );
+  }
 }
